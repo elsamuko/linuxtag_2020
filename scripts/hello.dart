b0b353e9f4aee3e8e87af61dcf09bf932b397ffc
Samuel Albrecht <elsamuko@gmail.com>
2020-08-19 23:04:14 +0200

    add: hello.dart

---
+++ scripts/hello.dart
@@ -0,0 +1,48 @@
+#!/usr/bin/env dart
+
+import 'dart:io';
+import 'dart:convert';
+
+void fileFunc() async {
+  {
+    File f = File("test");
+    f.writeAsStringSync("Hallo");
+  }
+
+  {
+    File f = File("test");
+    String content = f.readAsStringSync();
+    print(content);
+  }
+}
+
+Future<String> GET(String url) async {
+  HttpClientRequest request = await HttpClient().getUrl(Uri.parse(url));
+  HttpClientResponse response = await request.close();
+  return utf8.decoder.bind(response).first;
+}
+
+int main() {
+  // stdout
+  print("Hello Dart");
+
+  // lists
+  [1, 2, 3, 4, 5].forEach((element) {
+    print("Hello ${element}");
+  });
+
+  // maps
+  Map m = {"a": "1", "b": "3"};
+  m.forEach((key, value) {
+    print("${key} -> ${value}");
+  });
+
+  // files
+  fileFunc();
+
+  // HTTP
+  // print(await GET("http://icanhazip.com/"));
+
+  print("EOF");
+  return 0;
+}
