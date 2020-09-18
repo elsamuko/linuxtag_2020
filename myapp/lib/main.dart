d96cf584db2e576fa4cb24f0202fc863da75437d
Samuel Albrecht <elsamuko@gmail.com>
2020-09-18 19:33:11 +0200

    chg: hide debug banner

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
