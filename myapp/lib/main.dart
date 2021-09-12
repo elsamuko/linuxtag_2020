8df4350cf6513257b47d39d8450d88915ac72301
Samuel Albrecht <elsamuko@gmail.com>
2021-09-12 17:09:47 +0200

    fix: removed analysis hints

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'first.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
